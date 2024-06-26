200a6df7644c9ac9e8260d292d03e6caa58166ad
Gean Pedro da Silva <36330402+HimAndRobot@users.noreply.github.com>
2024-06-26 01:05:53 -0300

    Update main.blade.php

--- resources/views/livewire/main.blade.php
+++ resources/views/livewire/main.blade.php
@@ -15,30 +15,32 @@
                 </template>
             </ul>
         </div>
-        <div x-show="selectedOption != null"  class="w-9/12 h-full bg-[#1b1919] px-20 pt-[40px] overflow-y-scroll scrollbar-thumb-rounded-md scrollbar scrollbar-thumb-[#00000033] ">
-                <span class="text-white text-3xl"><span class="text-bold" x-text="selectedOption?.method"></span> <span class="text-[#a39c9b]" x-text="selectedOption?.path"></span></span>
-                <div class="w-full h-[40px] rounded-md bg-[#1d1d1d] mt-8 flex justify-center items-center">
-                    <span class="text-[#a39c9b]">From IP <span class="text-white" x-text="selectedOption?.ip"></span> at <span class="text-white" x-text="selectedOption?.created_at"></span></span>
-                </div>
-                <div class="my-5">
-                    <span class="text-white text-xl mt-[30px]">HEADERS</span>
-                </div>
-                <ul class="border-2 border-[#1d1d1d] border-b-0 rounded-2xln">
-                    <template x-for="(payload, index) in JSON.parse(selectedOption?.headers ?? '{}')">
-                        <li class="p-4 border-b-2 border-b-[#1d1d1d] flex w-full justify-between">
-                            <span class="text-white font-semibold pr-[400px] text-nowrap" x-text="index"></span>
-                            <span class="text-[#a39c9b] break-all" x-text="payload[0]"></span>
-                        </li>
-                    </template>
-                </ul>
-                <div class="my-5 flex justify-between items-center mt-[30px]">
-                    <span class="text-white text-xl">BODY</span>
-                    <button x-on:click="copyToClipboard" class="text-white cursor-pointer bg-[#212122] py-1 px-4 rounded-md flex items-center">
-                        Copy
-                        <svg class="ml-2" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg" size="10"><g clip-path="url(#prefix__clip0_342_4065)" stroke="white" stroke-linecap="round" stroke-linejoin="round"><path d="M3.5.5h8v8h-8v-8z"></path><path d="M1.5 3.5h-1v8h8v-1"></path></g><defs><clipPath id="prefix__clip0_342_4065"><path fill="white" transform="matrix(-1 0 0 1 12 0)" d="M0 0h12v12H0z"></path></clipPath></defs></svg>
-                    </button>
-                </div>
-                <pre class="w-full text-white rounded-md bg-[#1d1d1d] mt-8 p-5 mb-[200px]" x-html="syntaxHighlight(JSON.stringify(JSON.parse(selectedOption?.payload ?? '{}'), null, 2))"></pre>
+        <div class="w-9/12 h-full bg-[#1b1919] ">
+            <div x-show="selectedOption != null" x-transation  class="w-full h-full px-20 pt-[40px] overflow-y-scroll scrollbar-thumb-rounded-md scrollbar scrollbar-thumb-[#00000033]" >
+                    <span class="text-white text-3xl"><span class="text-bold" x-text="selectedOption?.method"></span> <span class="text-[#a39c9b]" x-text="selectedOption?.path"></span></span>
+                    <div class="w-full h-[40px] rounded-md bg-[#1d1d1d] mt-8 flex justify-center items-center">
+                        <span class="text-[#a39c9b]">From IP <span class="text-white" x-text="selectedOption?.ip"></span> at <span class="text-white" x-text="selectedOption?.created_at"></span></span>
+                    </div>
+                    <div class="my-5">
+                        <span class="text-white text-xl mt-[30px]">HEADERS</span>
+                    </div>
+                    <ul class="border-2 border-[#1d1d1d] border-b-0 rounded-2xln">
+                        <template x-for="(payload, index) in JSON.parse(selectedOption?.headers ?? '{}')">
+                            <li class="p-4 border-b-2 border-b-[#1d1d1d] flex w-full justify-between">
+                                <span class="text-white font-semibold pr-[400px] text-nowrap" x-text="index"></span>
+                                <span class="text-[#a39c9b] break-all" x-text="payload[0]"></span>
+                            </li>
+                        </template>
+                    </ul>
+                    <div class="my-5 flex justify-between items-center mt-[30px]">
+                        <span class="text-white text-xl">BODY</span>
+                        <button x-on:click="copyToClipboard" class="text-white cursor-pointer bg-[#212122] py-1 px-4 rounded-md flex items-center">
+                            Copy
+                            <svg class="ml-2" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg" size="10"><g clip-path="url(#prefix__clip0_342_4065)" stroke="white" stroke-linecap="round" stroke-linejoin="round"><path d="M3.5.5h8v8h-8v-8z"></path><path d="M1.5 3.5h-1v8h8v-1"></path></g><defs><clipPath id="prefix__clip0_342_4065"><path fill="white" transform="matrix(-1 0 0 1 12 0)" d="M0 0h12v12H0z"></path></clipPath></defs></svg>
+                        </button>
+                    </div>
+                    <pre class="w-full text-white rounded-md bg-[#1d1d1d] mt-8 p-5 mb-[200px]" x-html="syntaxHighlight(JSON.stringify(JSON.parse(selectedOption?.payload ?? '{}'), null, 2))"></pre>
+            </div>
         </div>
     </div>
 </div>
